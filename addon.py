6dfd0c5e060c77478b65d7f70340707a88168968
SLiX69 <asd2304@gmail.com>
2016-08-25 23:55:20 +0200

    added 'AddShow' and tiny changes

--- addon.py
+++ addon.py
@@ -19,21 +19,92 @@ api_key = addon.getSetting('api-key')
 
 
 vw_moni, vw_perc, vw_total = False, False, False
+vw_aired = False
 if addon.getSetting('view-moni') == 'true': vw_moni = True
 if addon.getSetting('view-perc') == 'true': vw_perc = True
 if addon.getSetting('view-total') == 'true': vw_total = True
+if addon.getSetting('view-aired') == 'true': vw_aired = True
 
-
-# check if ends with '/'
-host_url = base_url + '/api'
-
+if not base_url.endswith('/'):
+    base_url += '/'
+host_url = base_url + 'api'
 
 snr = SonarrAPI(host_url, api_key)
 
 
-
 def root():
     addDir(get_translation(30005), '', 'getAllShows', '', '', '')
+    addDir(get_translation(30009), '', 'addShow', '', '', '')
+
+
+def add_show(term=None):
+    xbmc.log('term')
+    xbmc.log(str(term))
+    if not term:
+        kb = xbmc.Keyboard()
+        kb.doModal()
+        if kb.isConfirmed() and kb.getText():
+            term = kb.getText()
+    # if user cancels, return
+    if not term:
+        return -1
+    # show lookup
+    shows = []
+    data = snr.lookup_series(term)
+    for show in data:
+        shows.append(show['title'])
+    if not shows:
+        # NOTHING FOUND NOTIFICATION
+        return -1
+    # open dialog for choosing show
+    dialog = xbmcgui.Dialog()
+    ret = dialog.select(get_translation(30210), shows)
+    if ret == -1:
+        return -1
+    xbmc.log('RET', level=0)
+    xbmc.log(str(ret))
+    # open dialog for choosing quality
+    quality_profile_id = list_quality_profiles()
+    if quality_profile_id == -1:
+        return -1
+    tvdbId = data[ret]['tvdbId']
+    title = data[ret]['title']
+    #seasons = data['seasons']
+    data = {
+        'tvdbId': tvdbId,
+        'title': title,
+        'qualityProfileId': quality_profile_id,
+        'rootFolderPath': snr.get_root_folder()[0]['path'],
+        # 'titleSlug': '',
+        # 'seasons': [],
+        'addOptions': {
+            'ignoreEpisodesWithFile': 'true',
+            'ignoreEpisodesWithoutFiles': 'true'
+        }
+    }
+    snr.add_series(data)
+
+
+def list_quality_profiles():
+    profiles = []
+    data = snr.get_quality_profiles()
+    for profile in data:
+        profile_id = profile['id']
+        profile_name = profile['name']
+        profiles.append({'name': profile_name, 'id': profile_id})
+    profiles_key_list = []
+    for profile in profiles:
+        profiles_key_list.append(profile['name'])
+    dialog = xbmcgui.Dialog()
+    ret = dialog.select(get_translation(30211), profiles_key_list)
+    if ret == -1:
+        return -1
+    id = profiles[ret]['id']
+    return id
+
+
+def add_new_show():
+    return True
 
 
 def list_shows(data):
@@ -55,12 +126,9 @@ def list_seasons(show_id):
     dir_show = get_appended_path(dir_shows, str(show_id))
     file_db = get_appended_path(dir_show, 'seasons.json')
     data = read_json(file_db)
-    xbmc.log('DATA LIST SEASON')
-    xbmc.log(str(data))
     for season in data:
         name = get_season_name(season)
         season_id = season['seasonNumber']
-        #name = 'Season %s' % str(season_id).zfill(2)
         addDir(name, show_id, 'getSeason', '', '', '', '', season_id)
 
 
@@ -72,9 +140,20 @@ def list_season(show_id, season_id):
         if episode['seasonNumber'] != int(season_id):
             continue
         else:
-            name = str(episode['episodeNumber']).zfill(2)
-            name += ' ' + episode['title'].encode('utf-8')
-            addDir(name, '', '', '', '', '')
+            name = get_episode_name(episode)
+            addDir(name, '', '', '', '', '', '', '', '')
+
+
+def get_episode_name(episode):
+    name = str(episode['seasonNumber']) + 'x'
+    name += str(episode['episodeNumber']).zfill(2)
+    if episode['hasFile']:
+        name += ' [COLOR FF3576F9]%s[/COLOR] ' % episode['title']
+    else:
+        name += ' [COLOR FFF7290A]%s[/COLOR] ' % episode['title']
+    if vw_aired and 'airDate' in episode:
+        name += (' [COLOR FF494545]%s[/COLOR]' % episode['airDate'])
+    return name
 
 
 def get_season_name(season):
@@ -91,6 +170,7 @@ def get_season_name(season):
         elif perc < 50:
             perc = '[COLOR FFF7290A]{}%[/COLOR]'.format(perc)  # red
         name += ' ' + str(perc)
+    # get episodes counter
     if vw_total:
         epi_count = str(season['statistics']['episodeCount'])
         epi_total_count = str(season['statistics']['totalEpisodeCount'])
@@ -104,7 +184,6 @@ def get_season_name(season):
             name += '[COLOR FF494545]%s[/COLOR]' % get_translation(30026)
     else:
         xbmc.log('VW MONI FALSE')
-
     return name
 
 
@@ -127,12 +206,14 @@ def get_all_episodes(show_id):
 
 
 
-def addDir(name, url, mode, iconimage, fanart, extra1, desc='', season=''):
+def addDir(name, url, mode, iconimage, fanart, extra1, desc='', season='', date=''):
     u = sys.argv[0] + "?url=" + str(url) + "&mode=" + str(mode) + "&name=" + str(name)# + "&extra1=" + extra1
     if season != '':
         u += "&season=" + str(season)
     ok = True
     item = xbmcgui.ListItem(name)
+    if date != '':
+        item.setInfo(type="video", infoLabels={"aired": date})
     item.setInfo(type="video", infoLabels={"title": name})
     item.setArt({'thumb': iconimage, 'fanart': fanart})
     xbmcplugin.addDirectoryItem(handle=pluginhandle, url=u, listitem=item, isFolder=True)
@@ -170,6 +251,8 @@ elif mode == 'getShow':
     get_show(url)
 elif mode == 'getSeason':
     list_season(url, season)
+elif mode == 'addShow':
+    add_show(url)
 else:
     root()
 
